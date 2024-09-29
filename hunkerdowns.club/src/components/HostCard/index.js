11ceda3ed8fcc373c1a224d4c52b96b561c449b3
Sean Idzenga <sean.idzenga@protonmail.com>
2024-09-28 23:39:10 -0400

    WIP on the host card

---
+++ hunkerdowns.club/src/components/HostCard/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './host-card.css';
+import mainImage from '../../img/synescape-temp.jpg';
+
+function HostCard() {
+
+  // TODO make/get svgs for the symbols in the top-right corner
+
+  // TODO make into props
+  const title = "Synescape"
+  const text1 = "Bonus to obscure and bizarre noise track drop rate AOE bass effect can stun enemies for two rounds"
+  const text2 = "Expert shitposter boosts dankness of all memes"
+  const text3 = "Crafts runes on armour and crafts party gear"
+
+  return(
+    <div className="host-card-component">
+      <div className="title">
+        <div className="doodad left"></div>
+        {title}
+        <div className="doodad right"></div>
+      </div>
+      <div className="portrait" style={{backgroundImage: `url(${mainImage})`}}/>
+      <div className="description-container">
+        <div className="description-background" />
+        <div className="doodad left">
+          <div className="doodad right small"/>
+        </div>
+        <div className="description-row">
+          {text1}
+        </div>
+        <div className="doodad right" />
+        <div className="doodad left">
+          <div className="doodad right small"/>
+        </div>
+        <div className="description-row">
+          {text2}
+        </div>
+        <div className="doodad right"/>
+        <div className="doodad left">
+          <div className="doodad right small"/>
+        </div>
+        <div className="description-row">
+          {text3}
+        </div>
+        <div className="doodad right"/>
+      </div>
+    </div>
+  );
+}
+
+export default HostCard;
