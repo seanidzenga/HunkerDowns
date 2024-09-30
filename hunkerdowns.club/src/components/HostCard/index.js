1cf06e97509e975ab8b2306668234bc5d4b99e80
Sean Idzenga <sean.idzenga@protonmail.com>
2024-09-30 14:00:04 -0400

    Added props for host cards

--- hunkerdowns.club/src/components/HostCard/index.js
+++ hunkerdowns.club/src/components/HostCard/index.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import './host-card.css';
-import mainImage from '../../img/synescape-temp.jpg';
 
-function HostCard() {
+function HostCard({title, portrait, highlight, type, text1, text2, text3}) {
 
   // TODO make/get svgs for the symbols in the top-right corner
 
-  // TODO make into props
-  const title = "Synescape"
-  const text1 = "Bonus to obscure and bizarre noise track drop rate AOE bass effect can stun enemies for two rounds"
-  const text2 = "Expert shitposter boosts dankness of all memes"
-  const text3 = "Crafts runes on armour and crafts party gear"
-
   return(
-    <div className="host-card-component">
+    <div className="host-card-component" style={{"--highlight": `${highlight || '#4f0056'}`}}>
       <div className="title">
         <div className="doodad left"></div>
         {title}
         <div className="doodad right">
-          <div className="type-icon bug"/>
+          <div className={`type-icon ${type}`}/>
         </div>
       </div>
-      <div className="portrait" style={{backgroundImage: `url(${mainImage})`}}/>
+      <div className="portrait" style={{backgroundImage: `url(${portrait})`}}/>
       <div className="description-container">
         <div className="description-background" />
         <div className="doodad left">
